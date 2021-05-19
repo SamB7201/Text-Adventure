65c1513b1aacca5a4918fbd332958632895d0609
SamB7201 <shb4@students.ptcollege.edu>
2021-05-19 13:02:31 -0400

    Almost Finished INT test
    INT Test almost finished needs to add the boss battle after test

--- MakeAGame/Program.cs
+++ MakeAGame/Program.cs
@@ -22,13 +22,24 @@ namespace MakeAGame
         static int monsterHealth = 0;
         static int monsterAttack = 0;
 
+        static string currentTest = "Beginner";
+
+        static bool room1_clear = false;
+        static bool room2_clear = false;
+        static bool room3_clear = false;
+
         static void Main(string[] args)
         {
             StartGame();
 
-            for (int Wave = 1; Wave <= 10; Wave++)
+            //for (int Wave = 1; Wave <= 10; Wave++)
+            //{
+            //    WaveAttack(Wave);
+            //}
+
+            for (int run = 1; run <= 3; run++)
             {
-                WaveAttack(Wave);
+                RoomSelect(room1_clear, room2_clear, room3_clear);
             }
 
             EndGame();
@@ -94,8 +105,8 @@ namespace MakeAGame
 
             Console.WriteLine($"\nDungeon Master: {playerName} you have chosen {role} with {playerAttack} Attack power, {playerHealth} Health," +
                 $" and {SkillPoints} Skill Points");
-            Console.WriteLine("Dungeon Master: Good Luck in the dungeoun!.... You'll need it.");
-            Console.WriteLine("*Door closes behind you, and text appears before your eyes*");
+            Console.WriteLine("Dungeon Master: Good Luck in the dungeoun!... You'll need it.");
+            Console.WriteLine("*The large doors closes behind you, and text appears before your eyes*");
         }
 
         private static void Fighting()
@@ -231,7 +242,7 @@ namespace MakeAGame
                     monsterAttack = 5;
 
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("You have entered the Dungeon of Waves. Clear the waves to leave alive!");
+                    Console.WriteLine("You have entered the Test of the Hero. Clear the Tests to prove yourself!");
                     Console.WriteLine($"A {monsterName} is approaching, Defend yourself {playerName}!");
                     Console.ForegroundColor = ConsoleColor.Gray;
 
@@ -335,7 +346,7 @@ namespace MakeAGame
                     break;
 
                 case 10:
-                    monsterName = "Demon King";
+                    monsterName = "Demon Lord";
                     monsterHealth = 200;
                     monsterAttack = 60;
 
@@ -346,6 +357,260 @@ namespace MakeAGame
                     Fighting();
                     break;
             }
+        }
+
+        private static void RoomSelect(bool room1, bool room2, bool room3)
+        {
+            Console.WriteLine($"\n*After defeating the {monsterName} text appears before your eyes*" +
+                $"\n{currentTest} test finished please select your next test.");
+
+            if (room1 != true)
+            {
+                Console.WriteLine("\nA door appears and has the depiction of Inteligence.");
+                Console.WriteLine("(Type INT to Enter)");
+            }
+            if (room2 != true)
+            {
+                Console.WriteLine("\nA door appears and has the depiction of Strength.");
+                Console.WriteLine("(Type STR to Enter)");
+            }
+            if (room3 != true)
+            {
+                Console.WriteLine("\nA door appears and has the depiction of Agility.");
+                Console.WriteLine("(Type AGI to Enter)");
+            }
+
+            string Select = Console.ReadLine();
+
+            if (room1 != true && Select.Equals("INT"))
+            {
+                IntTest();
+            }
+            if (room2 != true && Select.Equals("STR"))
+            {
+                StrTest();
+            }
+            if (room3 != true && Select.Equals("AGI"))
+            {
+                AgiTest();
+            }
+        }
+
+        private static void IntTest()
+        {
+            currentTest = "Inteligence";
+            int correct = 0;
+
+            Console.WriteLine("\n*You enter through the test doors then they close soon after, After a short silence a loud voice speaks out.*" +
+                "\nOhhh, I knew you'd come here next. This place is rather boring, so shall we play a game?" +
+                "\nThe rules are simple I will ask ten questions and if you get the majority correct you win and move on..." +
+                "\nBUT if I win this is as far as you go. *That last part was said rather ominously.*");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nNOTE: Word answers will be marked with _ for the number of words NOT letters. (ex: _ _ = A Sword)" +
+                "\nMultiple choice answers will be answered by entering the letter (ex: a)");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (int question = 1; question <= 10; question++)
+            {
+                string answer = "";
+                switch (question)
+                {
+                    case 1:
+                        Console.WriteLine("\nI have a Tail, a Head, but no Legs. What am I? _");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("coin"))
+                        {
+                            Console.WriteLine("Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect I was Coin.");
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nI Am The Beginning Of The End, And The End Of Before. What am I? _");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("e"))
+                        {
+                            Console.WriteLine("Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect I was E");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nI Am An Eye Set In A Blue Face. My Gaze Feeds The World. If I Go Blind So Does The World. _");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("sun"))
+                        {
+                            Console.WriteLine("Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect it was Sun");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nWhat Breathes, Consumes, And Grows, But Was And Never Will Be Alive?" +
+                            "\nA) An Undead." +
+                            "\nB) Fire." +
+                            "\nC) A Dragon.");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("b"))
+                        {
+                            Console.WriteLine("Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect it was Fire");
+                        }
+                        break;
+
+                    case 5:
+                        Console.WriteLine("\nWhat Falls but never Breaks, and what Breaks but never Falls? _ _ _");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("day and night") || answer.Equals("night and day"))
+                        {
+                            Console.WriteLine("WHAT!!! AHHH... Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect it was Night and Day");
+                        }
+                        break;
+
+                    case 6:
+                        Console.WriteLine("\nNo Matter Is Parched, No Matter If Rolled. No Matter If Magic, No Matter How Old. _");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("paper"))
+                        {
+                            Console.WriteLine("Fine Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect it was Paper");
+                        }
+                        break;
+
+                    case 7:
+                        Console.WriteLine("\nThe Rich Want It, The Poor Have It, And Both Will Perish If They Eat It?" +
+                            "\nA) Nothing." +
+                            "\nB) Death." +
+                            "\nC) Everything.");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("a"))
+                        {
+                            Console.WriteLine("Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect it was Nothing");
+                        }
+                        break;
+
+                    case 8:
+                        Console.WriteLine("\nPassed From Father To Son, And Shared Between Brothers, Though It Is Used More By Others. _ _");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("last name"))
+                        {
+                            Console.WriteLine("Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect it was Last Name");
+                        }
+                        break;
+
+                    case 9:
+                        Console.WriteLine("\nName Me And So Ye Shall Break Me. What am I without SPEAKING my name?");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals(""))
+                        {
+                            Console.WriteLine("Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You fool I was Silence you were not supposed to answer at all.");
+                        }
+                        break;
+
+                    case 10:
+                        Console.WriteLine("\nLast question. What happens when an Immovable Object meets an Irresitable Force?" +
+                            "\nA) The Object moves." +
+                            "\nB) The Force stops." +
+                            "\nC) They both move and do NOT move.");
+
+                        answer = Console.ReadLine().ToLower();
+                        if (answer.Equals("c"))
+                        {
+                            Console.WriteLine("Correct");
+                            correct++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HAHAHA Incorrect");
+                        }
+                        break;
+                }
+            }
+
+            if(correct <= 5) {
+                Console.WriteLine("\nHA HAHAHA I new I was a genius but to think these questions were so hard for you I can't hwlp but laugh." +
+                    "\nWell then I suppose I win... *You can't see the being that is speaking but you can almost feel it smiling from ear to ear.*");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Now then what do I do with you?..");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.ReadKey();
+                Console.WriteLine($"\n{playerName} has been captured by the Game Master of Inteligence. Press ENTER to restart.");
+
+                Console.ReadKey();
+                var info = new System.Diagnostics.ProcessStartInfo(Environment.GetCommandLineArgs()[0]);
+                System.Diagnostics.Process.Start(info);
+                Environment.Exit(0);
+            }
+            else
+            {
+
+                room1_clear = true;
+            }
+        }
+
+        private static void StrTest()
+        {
+            currentTest = "Strength";
+
+            room2_clear = true;
+        }
+
+        private static void AgiTest()
+        {
+            currentTest = "Agility";
+
+            room3_clear = true;
         }
 
         private static void EndGame()
